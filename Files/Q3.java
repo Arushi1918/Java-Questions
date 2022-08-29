9179b9ef3204fa35f3c1c7e6723eaf74ac89939e
Arushi <46837724+Arushi1918@users.noreply.github.com>
2022-08-29 07:52:55 +0530

    comments added

--- Files/Q3.java
+++ Files/Q3.java
@@ -12,11 +12,11 @@ public class Q3 {
         char[] buffer=null;
         try{
             BufferedReader reader=new BufferedReader(new FileReader(file));
-            buffer=new char[(int)file.length()];
+            buffer=new char[(int)file.length()];  //file.length is no. of lines in the file; buffer is character array of this length
             int i=0;
-            int c=reader.read();
+            int c=reader.read();  //c = no. of characters in current line
             while (c != -1) {
-                buffer[i++]=(char)c;
+                buffer[i++]=(char)c;  //buffer array stores no. of characters in each line
                 c=reader.read();
 
             }
